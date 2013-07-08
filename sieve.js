73531f56615b0913f03981f18169488c62f3e44c
Chris McKnight <cmckni3@gmail.com>
2013-07-08 06:55:13 -0500

    Modify sieve

--- sieve.js
+++ sieve.js
@@ -4,28 +4,27 @@ var sieve = function(n) {
   var arr = [0, 0];
   for (i = 2; i <= n; i++)
   {
-    arr.push(1);
+    arr.push(i);
   }
-    for (i = 2; i <= Math.sqrt(n); i++)
-    {
-      if (arr[i] !== 0)
-      {
-        for (j = i*i; j <= n; j += i)
-          arr[j] = 0;
-      }
-    }
-  var primeArr = [];
-  for (i = 2; i < arr.length; i++)
+  for (i = 2; i <= Math.sqrt(n); i++)
   {
-    if (arr[i] === 1)
-      primeArr.push(i);
+    if (arr[i] !== 0)
+    {
+      for (j = i*i; j <= n; j += i)
+      arr[j] = 0;
+    }
   }
+  var primeArr = arr.filter(function(num){
+    if (num !== 0)
+    return true;
+    return false;
+  });
   return primeArr;
 };
 
 // Print to console
 var fmt = function(arr) {
-    return arr.join(" ");
+  return arr.join(" ");
 };
 
 var k = 100;
